9027dcc1e5f73466068ebef649b2b5d1a2aab097
AlLNaumov <allnaumob@inno.tech>
2024-07-14 21:40:56 +0300

    Внесены изменения в файл Main.java - получение двух чисел в консоль, проведение с ними арифметических операций и вывод результата в консоль

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Первое число из консоли
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        // Второе число из консоли
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        // Заранее введенные данные
+        //int firstNumber =5;
+        //int secondNumber = 10;
+
+        //Вычисляем
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        //Выводим результат
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.printf("Частное: %.2f\n", quotient);
     }
 }
